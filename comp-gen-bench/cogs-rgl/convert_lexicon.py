2c5349f156721bf37c62ac133c594d4482634280
Anssi Moisio <anssi.moisio@aalto.fi>
2026-03-14 13:54:52 +0200

    make cogs-rgl/convert_lexicon.py usable as a module

--- comp-gen-bench/cogs-rgl/convert_lexicon.py
+++ comp-gen-bench/cogs-rgl/convert_lexicon.py
@@ -1,7 +1,6 @@
 """Convert the lexicon from cogs-lexicon to GF format."""
 #!/usr/bin/env python3
-import os
-from cogs_lexicon import *
+from utils.cogs_lexicon import *
 
 N = animate_nouns + inanimate_nouns + on_nouns + in_nouns + beside_nouns
 PN = proper_nouns
@@ -19,9 +18,9 @@ for word_list, lemma_word_list in zip(
         [VUnerg, VUnacc, V2, V3, VS, VV]):
     for word in word_list:
         lemma_word_list.append(verbs_lemmas[word])
-    print("------")
-    for w in set(lemma_word_list):
-        print(w)
+    # print("------")
+    # for w in set(lemma_word_list):
+    #     print(w)
 
 # missing from cogs_lexicon
 VUnerg.append("bake")
@@ -31,7 +30,6 @@ verbs_lemmas["baked"] = "bake"
 # adjectives
 A = ["big", "small", "red", "blue", "green", "happy", "sad", "angry",
      "tall", "short", "young", "old", "fast", "slow", "loud", "quiet"]
-
 
 lexs = {
     "N": sorted(list(set(N))),
@@ -52,23 +50,6 @@ lexs = {
 
 exclude = ["want"]
 
-# abstract lexicon
-with open("CogsLexicon.gf", "w", encoding="utf-8") as f:
-    f.write("abstract CogsLexicon = Cogs, Structural ** {\n")
-    f.write("data\n")
-
-    for wordclass, wordlist in lexs.items():
-        for word in wordlist:
-            if word in exclude:
-                continue
-            f.write(f"    {word.lower()}_{wordclass} \t\t: {wordclass.split('_')[-1]} ;\n")
-        f.write("\n")
-
-    f.write("    beside_Prep : Prep ;\n")
-    f.write("\n}\n")
-
-# concrete lexicon LexiconEng
-# only lins needed, no lincat
 linfun = { # strings that need to be formatted with the word
     # constructors for regular and irregular verbs
     "N": ("mkN {}", "mkN {}"),
@@ -109,58 +90,72 @@ verb_base2infls["redden"] = ("reddened", "reddened")
 
 
 
+if __name__ == "__main__":
 
-with open("CogsLexiconEng.gf", "w", encoding="utf-8") as f:
-    f.write("concrete CogsLexiconEng of CogsLexicon = CogsEng ** " \
-            + "open ParadigmsEng, IrregEng, Prelude in {\n")
-    f.write("lin\n")
-    for wordclass, wordlist in lexs.items():
-        for word in wordlist:
-            if word in exclude:
-                continue
-            if any(wordclass.startswith(prefix) for prefix in ["to_V", "V"]) and \
-                    (word in verb_base2infls and \
-                    verb_base2infls[word][0] != word + "ed" and \
-                    verb_base2infls[word][0] != word[:-1] + "ed") or word in ["shorten", "redden"]:
-                    # verb_base2infls[word][0] != word + word[-1] + "ed":
-                forms = f'"{word}" "{verb_base2infls[word][0]}" "{verb_base2infls[word][1]}"'
-                linf = linfun[wordclass][1]
-            else:
-                forms = f'"{word}"'
-                linf = linfun[wordclass][0]
+    # abstract lexicon
+    with open("CogsLexicon.gf", "w", encoding="utf-8") as f:
+        f.write("abstract CogsLexicon = Cogs, Structural ** {\n")
+        f.write("data\n")
+        for wordclass, wordlist in lexs.items():
+            for word in wordlist:
+                if word in exclude:
+                    continue
+                f.write(f"    {word.lower()}_{wordclass} \t\t: {wordclass.split('_')[-1]} ;\n")
+            f.write("\n")
+        f.write("    beside_Prep : Prep ;\n")
+        f.write("\n}\n")
 
-            f.write(f"    {word.lower()}_{wordclass} \t\t= {linf.format(forms)} ;\n")
-        f.write("\n")
+    # concrete lexicon LexiconEng
+    # only lins needed, no lincat
+    with open("CogsLexiconEng.gf", "w", encoding="utf-8") as f:
+        f.write("concrete CogsLexiconEng of CogsLexicon = CogsEng ** " \
+                + "open ParadigmsEng, IrregEng, Prelude in {\n")
+        f.write("lin\n")
+        for wordclass, wordlist in lexs.items():
+            for word in wordlist:
+                if word in exclude:
+                    continue
+                if any(wordclass.startswith(prefix) for prefix in ["to_V", "V"]) and \
+                        (word in verb_base2infls and \
+                        verb_base2infls[word][0] != word + "ed" and \
+                        verb_base2infls[word][0] != word[:-1] + "ed") or word in ["shorten", "redden"]:
+                        # verb_base2infls[word][0] != word + word[-1] + "ed":
+                    forms = f'"{word}" "{verb_base2infls[word][0]}" "{verb_base2infls[word][1]}"'
+                    linf = linfun[wordclass][1]
+                else:
+                    forms = f'"{word}"'
+                    linf = linfun[wordclass][0]
 
-    # structural words missing from gf-rgl/src/abstract/Structural.gf
+                f.write(f"    {word.lower()}_{wordclass} \t\t= {linf.format(forms)} ;\n")
+            f.write("\n")
 
-    f.write("    beside_Prep = mkPrep \"beside\" ;\n")
+        # structural words missing from gf-rgl/src/abstract/Structural.gf
+        f.write("    beside_Prep = mkPrep \"beside\" ;\n")
 
-    f.write("oper")
-    f.write('    aboutP = mkPrep "about" ;\n')
-    f.write('    atP = mkPrep "at" ;\n')
-    f.write('    forP = mkPrep "for" ;\n')
-    f.write('    fromP = mkPrep "from" ;\n')
-    f.write('    inP = mkPrep "in" ;\n')
-    f.write('    onP = mkPrep "on" ;\n')
-    f.write('    toP = mkPrep "to" ;\n')
-    f.write('    besideP = mkPrep "beside" ;\n')
+        f.write("oper")
+        f.write('    aboutP = mkPrep "about" ;\n')
+        f.write('    atP = mkPrep "at" ;\n')
+        f.write('    forP = mkPrep "for" ;\n')
+        f.write('    fromP = mkPrep "from" ;\n')
+        f.write('    inP = mkPrep "in" ;\n')
+        f.write('    onP = mkPrep "on" ;\n')
+        f.write('    toP = mkPrep "to" ;\n')
+        f.write('    besideP = mkPrep "beside" ;\n')
 
-    f.write("\n}\n")
+        f.write("\n}\n")
 
 
+    ##### for the logical forms
+    with open("CogsLexiconLF.gf", "w", encoding="utf-8") as f:
+        f.write("concrete CogsLexiconLF of CogsLexicon = open Prelude in {\n")
+        f.write("lin\n")
+        for wordclass, wordlist in lexs.items():
+            for word in wordlist:
+                f.write(f'    {word.lower()}_{wordclass} \t\t= ss "{word}" ;\n')
+            f.write("\n")
 
-##### for the logical forms
-with open("CogsLexiconLF.gf", "w", encoding="utf-8") as f:
-    f.write("concrete CogsLexiconLF of CogsLexicon = open Prelude in {\n")
-    f.write("lin\n")
-    for wordclass, wordlist in lexs.items():
-        for word in wordlist:
-            f.write(f'    {word.lower()}_{wordclass} \t\t= ss "{word}" ;\n')
-        f.write("\n")
+        f.write("    beside_Prep = ss \"beside\" ;\n")
+        f.write("    in_Prep = ss \"in\" ;\n")
+        f.write("    on_Prep = ss \"on\" ;\n")
 
-    f.write("    beside_Prep = ss \"beside\" ;\n")
-    f.write("    in_Prep = ss \"in\" ;\n")
-    f.write("    on_Prep = ss \"on\" ;\n")
-
-    f.write("\n}\n")
+        f.write("\n}\n")
